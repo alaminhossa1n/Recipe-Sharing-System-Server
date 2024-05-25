1e1ab7a381b8960fd2f0e67eccd203eb001ff1d8
unknown <alaminatghoria@gmail.com>
2024-05-25 21:28:33 +0600

    jwt added

--- src/app/modules/Users/user.service.ts
+++ src/app/modules/Users/user.service.ts
@@ -1,12 +1,47 @@
+import config from "../../config";
 import { TUser } from "./user.interface";
 import UserModel from "./user.model";
+import jwt from "jsonwebtoken";
 
 const createUserInToDB = async (payload: TUser) => {
-    const result = await UserModel.create(payload);
-    return result;
-  };
-  
-  export const UserServices = {
-    createUserInToDB,
-  };
-  
+  try {
+    const isUserExist = await UserModel.findOne({ email: payload?.email });
+
+    const jwtPayload = {
+      email: payload?.email,
+      displayName: payload?.displayName,
+    };
+
+    const accessToken = jwt.sign(jwtPayload, config.jwt_secret as string, {
+      expiresIn: "10d",
+    });
+
+    if (!isUserExist) {
+      const result = await UserModel.create(payload);
+      return {
+        user: result,
+        token: accessToken,
+      };
+    }
+
+    return {
+      user: {
+        email: payload?.email,
+        displayName: payload?.displayName,
+      },
+      token: accessToken,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      success: false,
+      statusCode: 500,
+      message: "Internal server error",
+      data: null,
+    };
+  }
+};
+
+export const UserServices = {
+  createUserInToDB,
+};
